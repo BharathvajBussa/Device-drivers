db747a36f5bdcb5140ea93d71acadc71dbc083e2
Bharathvaj <bussabharathvaj1@gmail.com>
2021-08-18 16:48:22 +0100

    incomplete

--- native/Day5/q5-3/caldriver.c
+++ native/Day5/q5-3/caldriver.c
@@ -6,85 +6,93 @@
 #include<linux/cdev.h>
 #include<linux/uaccess.h>
 #include"caldriver.h"
-
-
-int za;
-int ud;
-int zb;
-int zc;
-int zd;
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 //Add function prototype
-int Add_open(struct inode *inode,struct file *filep);
-int Add_release(struct inode *inode,struct file *filep);
-ssize_t Add_write(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
-ssize_t Add_read(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
+int open_add(struct inode *inode,struct file *filep);
+int release_add(struct inode *inode,struct file *filep);
+ssize_t write_add(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
+ssize_t read_add(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
+
 //Sub function prototypes
-int Sub_open(struct inode *inode,struct file *filep);
-int Sub_release(struct inode *inode,struct file *filep);
-ssize_t Sub_write(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
-ssize_t Sub_read(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
+int open_sub(struct inode *inode,struct file *filep);
+int release_sub(struct inode *inode,struct file *filep);
+ssize_t write_sub(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
+ssize_t read_sub(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
 //Mul function prototype
-int Mul_open(struct inode *inode,struct file *filep);
-int Mul_release(struct inode *inode,struct file *filep);
-ssize_t Mul_write(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
-ssize_t Mul_read(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
+int open_mul(struct inode *inode,struct file *filep);
+int release_mul(struct inode *inode,struct file *filep);
+ssize_t write_mul(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
+ssize_t read_mul(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
 //div function prototype
-int Div_open(struct inode *inode,struct file *filep);
-int Div_release(struct inode *inode,struct file *filep);
-ssize_t Div_write(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
-ssize_t Div_read(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
-
-//structure that stores operations provided by the driver
-struct file_operations fops =
-{
-	.owner=THIS_MODULE,
-	.open=Add_open,
-	.read=Add_read,
-	.write=Add_write,	
-	.release=Add_release,
-};
-
-
-//structure for a character driver
-struct cdev *my_cdev;
-
-//creating device number
-dev_t Mydev=MKDEV(255,0);
-
+int open_div(struct inode *inode,struct file *filep);
+int release_div(struct inode *inode,struct file *filep);
+ssize_t write_div(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp);
+ssize_t read_div(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp);
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+struct cdev *AddDev;
+struct cdev *SubDev;
+struct cdev *MulDev;
+struct cdev *DivDev;
+dev_t Mydev;
+dev_t add;
+dev_t sub;
+dev_t mul;
+dev_t div;
 //init function
-static int __init CharDevice_init(void)
+static int __init calculator_init(void)
 {
 	int result;
-	printk("Major number is %d\n Minor number is %d\n",MAJOR(Mydev),MINOR(Mydev));
-	result=register_chrdev_region(Mydev,4,"Operation");//register device region
-	if(result<0)
+	result=register_chrdev_region(Mydev,4,"Operations");//register device region
+	add=MKDEV(255,0);
+	sub=MKDEV(255,1);
+	mul=MKDEV(255,2);
+	div=MKDEV(255,3);
+	//memory allocation
+	AddDev=cdev_alloc();
+	SubDev=cdev_alloc();
+	MulDev=cdev_alloc();
+	DivDev=cdev_alloc();
+	//linking fops
+	AddDev->ops=&fops_add;
+	SubDev->ops=&fops_sub;
+	MulDev->ops=&fops_mul;
+	DivDev->ops=&fops_div;
+	int res_add;
+	res_add=cdev_add(AddDev,Mydev,1);//Notify kernel about new device
+	if(res_add<0)
 	{
-		printk(KERN_ALERT "The Region requested is not obtainable\n");
+		printk(KERN_ALERT "\nThe add is not allocated\n");
 		return(-1);
 	}
-	my_cdev=cdev_alloc();//allocate memory to chardev structure
-	my_cdev->ops=&fops;//link fops to char device
-
-	result=cdev_add(my_cdev,Mydev,4);//Notify kernel about new device
-	if(result<0)
+	int res_sub;
+	res_sub=cdev_sub(SubDev,Mydev,1);
+	if(res_sub<0)
 	{
-		printk(KERN_ALERT "The Char device is not created\n");
-		unregister_chrdev_region(Mydev,4);
-		return(-1);
+		printk(KERN_ALERT "\nThe sub is not allocated\n");
+		return (-1);
+	}
+	int res_mul;
+	res_mul=cdev_mul(MulDev,Mydev,1);
+	if(res_mul<0)
+	{
+		printk(KERN_ALERT "\n The mul is not allocated\n");
+		return (-1);
+	}
+	int res_div;
+	res_div=cdev_div(DivDev,Mydev,1);
+	if(res_div<0)
+	{
+		printk(KERN_ALERT "\n The div is not allocated\n");
+		return (-1);
 	}
 	return 0;
 }
-
-//clean up function
-void __exit CharDevice_exit(void)
-{
-	printk("\n Major number is %d\n Minor number is %d\n",MAJOR(Mydev),MINOR(Mydev));
-	unregister_chrdev_region(Mydev,4);//unregister device number and device
-	cdev_del(my_cdev);
-	printk(KERN_ALERT "\nI have unregistered...\n");
-}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 //add read function
-ssize_t Add_read(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp)
+ssize_t read_add(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp)
 {
 	unsigned long result;
 	ssize_t retval;
@@ -104,7 +112,7 @@ ssize_t Add_read(struct file *filep,char __user *Ubuff,size_t count,loff_t *offp
 
 }
 
-ssize_t Add_write(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp)
+ssize_t write_add(struct file *filep,const char __user *Ubuff,size_t count,loff_t *offp)
 {
 	unsigned long result;
 	ssize_t retval;
@@ -124,59 +132,76 @@ ssize_t Add_write(struct file *filep,const char __user *Ubuff,size_t count,loff_
 	}
 	return 0;
 }
-
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 //Open system call 
-int Add_open(struct inode *inode,struct file *filep)
+int open_add(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Add open system call\n");
 	return 0;
 }
 
 //close system call
-int Add_release(struct inode *inode,struct file *filep)
+int release_add(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Add close system call\n");
 	return 0;
 }
 
 //sub open call
-int Sub_open(struct inode *inode,struct file *filep)
+int open_sub(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Sub open system call\n");
 	return 0;
 }
 //sub close call
-int Sub_release(struct inode *inode,struct file *filep)
+int release_sub(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Sub close system call\n");
 	return 0;
 }
 //mul open call
-int Mul_open(struct inode *inode,struct file *filep)
+int open_mul(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Mul open system call\n");
 	return 0;
 }
 //mul close call
-int Mul_release(struct inode *inode,struct file *filep)
+int release_mul(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Mul close system call\n");
 	return 0;
 }
 //div open call
-int Div_open(struct inode *inode,struct file *filep)
+int open_div(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Div open system call\n");
 	return 0;
 }
 //div close call
-int Div_release(struct inode *inode,struct file *filep)
+int release_div(struct inode *inode,struct file *filep)
 {
 	printk(KERN_ALERT "\n In Div close system call\n");
 	return 0;
 }
-module_init(CharDevice_init);
-module_exit(CharDevice_exit);
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//clean up function
+static void calculator_clean(void)
+{
+	//delete the device locations
+	cdev_del(AddDev);
+	cdev_del(SubDev);
+	cdev_del(MulDev);
+	cdev_del(DivDev);
+	cdev_del(Mydev);
+	//unregister from the kernel space
+	unregister_chrdev_region(my_device_number, 4);
+	printk(KERN_ALERT "I have unregistered\n");
+}
+
+module_init(calculator_init);
+module_exit(calculator_clean);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Bharathvaj");
